6c5f25caed00974ceae33b0c93f825b6e0856f58
LukPor375 <lporebski97@gmail.com>
2022-01-16 19:05:54 +0100

    first commit

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,53 +1,82 @@
 #!/bin/bash
-#title        : install_firefix_dev.sh
-#description  : This script will install Firefox Developer Edition x86_64
-#author       : Alexandre ZANNi
-#date         : 2016/08/30
-#version      : 0.1
-#bash_version : GNU bash, version 4.2.47(1)-release (x86_64-suse-linux-gnu)
-#=============================================================================
 
-#
-# Warning: Require sudo to move files into /opt/
+VERSION='15.0.1'
+FILE="firefox-$VERSION.tar.bz2"
+ARCH=`uname -m`
+RELEASES='ftp.mozilla.org/pub/mozilla.org/firefox/releases'
+TO='/var/lib'
+FILE_PATH="$TO/$FILE"
+EXEC='firefox/firefox'
+GREEN='\033[0;32m'
+RED='\033[1;31m'
+NO_COLOR='\033[1;0m'
 
-# You can choose lang
-lang="en-US"
-url="https://download.mozilla.org/?product=firefox-aurora-latest-ssl&os=linux64&lang=$lang"
+export LANGUAGE='en-US'
+export LC_ALL='en_US.UTF-8'
+export LANG='en-US'
 
-# Get filename
-solved_URL=$(curl -L --head -w '%{url_effective}' $url 2>/dev/null | tail -n1)
-filename_with_ext=$(echo $solved_URL | sed 's/.*\///')
-
-# Download the file
-wget $url -P $HOME --trust-server-names
-
-# Remove archive extension (ex: bz2)
-filename=$(echo $filename_with_ext | sed 's/\.[^.]*$//')
-# Remove tar extension
-filename=$(echo $filename | sed 's/\.[^.]*$//')
-
-temp_folder="$HOME/$filename/"
-sub_folder="firefox/"
-absolut_filename="$HOME/$filename_with_ext"
-if [ ! -d $temp_folder ]
-then
-  # Create a temporary folder, extract archive
-  mkdir -p $temp_folder && tar xaf $absolut_filename -C $temp_folder
-  rm -rf "/opt/firefox-dev/" # remove if updating and don't prompt if not existing
-  mv -f "$temp_folder$sub_folder" "/opt/firefox-dev/"
-  # Create a shortcut
-  cat > ~/.local/share/applications/firefox-dev.desktop << EOF
-[Desktop Entry]
-Name=Firefox Developer
-GenericName=Firefox Developer Edition
-Exec=/opt/firefox-dev/firefox
-Terminal=false
-Icon=/opt/firefox-dev/browser/icons/mozicon128.png
-Type=Application
-Categories=Application;Network;X-Developer;
-Comment=Firefox Developer Edition Web Browser
-EOF
-  # Remove the temporary folder & archive
-  rmdir $temp_folder
-  rm $absolut_filename
+if [ -d "$TO/firefox" ]; then
+  echo -e "${GREEN}Removing the current installed version of Firefox...${NO_COLOR}"
+  sudo apt-get purge firefox firefox-globalmenu firefox-gnome-support
+  sudo apt-get autoremove
 fi
+
+echo -e "${GREEN}Going to home${NO_COLOR}"
+cd $HOME
+
+if [ ! -e $FILE ]; then
+  echo -e "${GREEN}Downloading the file ($RELEASES/$VERSION/linux-$ARCH/$LANG/$FILE)...${NO_COLOR}"
+  wget "$RELEASES/$VERSION/linux-$ARCH/$LANG/$FILE"
+fi
+
+if [ ! -e $FILE ]; then
+  echo -e "${RED}The download was failed!${NO_COLOR}"
+  echo -e "${RED}Check if that version was released on site: http://$RELEASES ${NO_COLOR}"
+  exit 1
+fi
+
+echo -e "${GREEN}Extracting the zip file${NO_COLOR}"
+
+tar -jxf $FILE
+
+echo -e "${GREEN}Moving the folder firefox to /var/lib${NO_COLOR}"
+
+if [ -d "$TO/firefox" ]; then
+  sudo rm -rf $TO/firefox
+fi
+
+sudo mv -f firefox $TO/firefox
+
+echo -e "${GREEN}Applying the permissions${NO_COLOR}"
+
+sudo chmod +x $TO/$EXEC
+
+echo -e "${GREEN}Creating the /usr/lib/firefox symbolic link to /var/lib${NO_COLOR}"
+
+cd /usr/lib
+
+if [ -f 'firefox' ]; then
+  sudo rm firefox
+fi
+
+sudo ln -s /var/lib/firefox firefox
+
+echo -e "${GREEN}Downloading the firefox.sh launcher${NO_COLOR}"
+
+cd $TO/firefox
+
+wget "https://gist.github.com/wbotelhos/3899743/raw/e769802e9242999ab837e6620f093e110fb10f5e/firefox.sh"
+
+chmod +x firefox.sh
+
+echo -e "${GREEN}Creating the launcher link on /usr/bin${NO_COLOR}"
+
+cd /usr/bin
+
+if [ -f 'firefox' ]; then
+  sudo rm firefox
+fi
+
+sudo ln -s $TO/firefox/firefox.sh firefox
+
+exit 0
